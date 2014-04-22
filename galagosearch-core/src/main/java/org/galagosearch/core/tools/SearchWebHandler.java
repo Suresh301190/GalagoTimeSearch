5df5995fa5e87fb3e2f2b971264c802e44af3edf
chaitz24 <chaitz24@gmail.com>
2014-04-23 00:24:41 +0530

    UI_Tooltip
    ToolTip Added

--- galagosearch-core/src/main/java/org/galagosearch/core/tools/SearchWebHandler.java
+++ galagosearch-core/src/main/java/org/galagosearch/core/tools/SearchWebHandler.java
@@ -2,6 +2,8 @@
 
 package org.galagosearch.core.tools;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,9 +14,11 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Set;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.galagosearch.core.parse.Document;
 import org.galagosearch.core.tools.Search.SearchResult;
 import org.galagosearch.core.tools.Search.SearchResultItem;
@@ -99,6 +103,7 @@ public class SearchWebHandler extends AbstractHandler {
         if (document == null) {
             response.setStatus(response.SC_NOT_FOUND);
         } else {
+        	
             response.setContentType("text/xml");
             PrintWriter writer = response.getWriter();
             String snippet = search.getSummary(document, queryTerms);
@@ -136,8 +141,38 @@ public class SearchWebHandler extends AbstractHandler {
         response.setContentType("image/png");
         retrieveImage(output,request);
     }
+    
+    public void handleJs(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+    	OutputStream output = response.getOutputStream();
+        response.setContentType("text/javascript");
+        retrieveJs(output,request);
+		
+	}
 
-    public void handleSearch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void retrieveJs(OutputStream output, HttpServletRequest request) throws IOException {
+		// TODO Auto-generated method stub
+    	InputStream css = getClass().getResourceAsStream(request.getPathInfo());
+        Utility.copyStream(css, output);
+        output.close();
+		
+	}
+    public void handleCs(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+    	OutputStream output = response.getOutputStream();
+        response.setContentType("text/javascript");
+        retrieveJs(output,request);
+		
+	}
+
+    public void retrieveCs(OutputStream output, HttpServletRequest request) throws IOException {
+		// TODO Auto-generated method stub
+    	InputStream css = getClass().getResourceAsStream(request.getPathInfo());
+        Utility.copyStream(css, output);
+        output.close();
+		
+	}
+	public void handleSearch(HttpServletRequest request, HttpServletResponse response) throws Exception {
         SearchResult result = performSearch(request);
         response.setContentType("text/html");
         String displayQuery = scrub(request.getParameter("q"));
@@ -146,35 +181,12 @@ public class SearchWebHandler extends AbstractHandler {
         PrintWriter writer = response.getWriter();
         writer.append("<html>\n");
         writer.append("<head>\n");
-        //writer.append("<link rel=\"stylesheet\" href=\"jquery.tooltip.css\" type=\"text/css\" />");
-        /*writer.append("div.jquery-gdakram-tooltip {"+
-  "width: 340px;"+
-  "color: white;"+
-  "font-size: 12px;"+
-	"position: absolute;"+
-	"z-index: 10000;"+
-	"top: 0px;"+
-	"left: 0px;"+
-	"display: none;}"+
-
-
-"div.jquery-gdakram-tooltip div.content {-moz-border-radius: 1em;  -webkit-border-radius: 1em;  border-radius: 1em;  background-color: #671329;  width: 280px;  min-height: 200px;  float: left;  padding: 10px;}"+
-"div.jquery-gdakram-tooltip div.content h1 {  font-size: 16px;  border-bottom: 1px solid #C4C4C4;  padding-bottom: 5px;}"+
-
-"div.jquery-gdakram-tooltip div.up_arrow {  background : url('images/up_arrow.png') 60px 0px no-repeat;  width: 100%;  height: 20px;}"+
-
-"div.jquery-gdakram-tooltip div.down_arrow {  background : url('images/down_arrow.png') 60px 0px no-repeat;  width: 100%;  height: 20px;}"+
-
-"div.jquery-gdakram-tooltip div.left_arrow {  height: 100%;}"+
-
-"div.jquery-gdakram-tooltip div.left_arrow {  float:left;  background : url('images/left_arrow.png') 0px 0px no-repeat;  width: 20px;  height: 20px;  position: relative;  top: 40px;  left: 0px;}"+
-
-"div.jquery-gdakram-tooltip div.right_arrow {  float:right;  background : url('images/right_arrow.png') 0px 0px no-repeat;  width: 20px;  height: 20px;  position: relative;  top: 40px;  right: 20px;}");*/
-        writer.append("<script src=\"jquery.tooltip.js\"></script>");
-        writer.append("<script src=\"jquery.tooltip.min.js\"></script>");
-        writer.append("<script type=\"text/javascript\">$(document).ready(function(){ $j = jQuery.noConflict(); $j(document).ready(function(){$j(\"div.result\").tooltip();});});</script>");
-        writer.append(String.format("<title>%s - Galago Search</title>\n", displayQuery));
         writeStyle(writer);
+        //writer.append("<link type=\"text/css\" rel=\"stylesheet\" href=\"/css1/jquery.tooltip.css\"  />");
+        //writer.append("<script type=\"text/javascript\" src=\"http://code.jquery.com/jquery.min.js\"></script>\n");
+        //writer.append("<script type=\"text/javascript\" src=\"/js1/jquery.tooltip.js\"></script>\n");
+        //writer.append("<script type=\"text/javascript\">$(document).ready(function(){ $j = jQuery.noConflict(); $j(document).ready(function(){$j(\"div#result\").tooltip();});});</script>");
+        writer.append(String.format("<title>%s - Galago Search</title>\n", displayQuery));
         writer.append("<script type=\"text/javascript\">\n");
         writer.append("function toggleDebug() {\n");
         writer.append("   var object = document.getElementById('debug');\n");
@@ -186,7 +198,67 @@ public class SearchWebHandler extends AbstractHandler {
         writer.append("}\n");
         writer.append("</script>\n");
         writer.append("</head>\n<body>\n");
+        writer.append("<div id=\"dhtmltooltip\"></div>\n"+
 
+"<script type=\"text/javascript\">\n"+
+
+"var offsetxpoint=-60\n"+
+"var offsetypoint=20\n"+
+"var ie=document.all \n"+
+"var ns6=document.getElementById && !document.all \n"+
+"var enabletip=false \n"+
+"if (ie||ns6) \n"+
+"var tipobj=document.all? document.all[\"dhtmltooltip\"] : document.getElementById? document.getElementById(\"dhtmltooltip\") : \"\" \n"+
+
+"function ietruebody(){ \n"+
+"return (document.compatMode && document.compatMode!=\"BackCompat\")? document.documentElement : document.body \n"+
+"} \n"+
+
+"function ddrivetip(thetext, thecolor, thewidth){ \n"+
+"if (ns6||ie){ \n"+
+"if (typeof thewidth!=\"undefined\") tipobj.style.width=thewidth+\"px\" \n"+
+"if (typeof thecolor!=\"undefined\" && thecolor!=\"\") tipobj.style.backgroundColor=thecolor \n"+
+"tipobj.innerHTML=thetext \n"+
+"enabletip=true \n"+
+"return false \n"+
+"} \n"+
+"} \n"+
+
+"function positiontip(e){ \n"+
+"if (enabletip){ \n"+
+"var curX=(ns6)?e.pageX : event.clientX+ietruebody().scrollLeft; \n"+
+"var curY=(ns6)?e.pageY : event.clientY+ietruebody().scrollTop; \n"+
+"var rightedge=ie&&!window.opera? ietruebody().clientWidth-event.clientX-offsetxpoint : window.innerWidth-e.clientX-offsetxpoint-20 \n"+
+"var bottomedge=ie&&!window.opera? ietruebody().clientHeight-event.clientY-offsetypoint : window.innerHeight-e.clientY-offsetypoint-20 \n"+
+
+"var leftedge=(offsetxpoint<0)? offsetxpoint*(-1) : -1000 \n"+
+"if (rightedge<tipobj.offsetWidth) \n"+
+"tipobj.style.left=ie? ietruebody().scrollLeft+event.clientX-tipobj.offsetWidth+\"px\" : window.pageXOffset+e.clientX-tipobj.offsetWidth+\"px\" \n"+
+"else if (curX<leftedge) \n"+
+"tipobj.style.left=\"5px\" \n"+
+"else \n"+
+"tipobj.style.left=curX+offsetxpoint+\"px\" \n"+
+"if (bottomedge<tipobj.offsetHeight) \n"+
+"tipobj.style.top=ie? ietruebody().scrollTop+event.clientY-tipobj.offsetHeight-offsetypoint+\"px\" : window.pageYOffset+e.clientY-tipobj.offsetHeight-offsetypoint+\"px\" \n"+
+"else \n"+
+"tipobj.style.top=curY+offsetypoint+\"px\" \n"+
+"tipobj.style.visibility=\"visible\" \n"+
+"} \n"+
+"} \n"+
+
+"function hideddrivetip(){ \n"+
+"if (ns6||ie){ \n"+
+"enabletip=false \n"+
+"tipobj.style.visibility=\"hidden\" \n"+
+"tipobj.style.left=\"-1000px\" \n"+
+"tipobj.style.backgroundColor='' \n"+
+"tipobj.style.width='' \n"+
+"} \n"+
+"} \n"+
+
+"document.onmousemove=positiontip \n"+
+
+"</script> \n");
         writer.append("<div id=\"header\">\n");
         writer.append("<table ><tr >");
         writer.append("<td width=\"15%\"><a href=\"http://www.galagosearch.org\">" +"<img src=\"/images/galago.png\" width=\"100%\"></a></td>");
@@ -209,8 +281,9 @@ public class SearchWebHandler extends AbstractHandler {
         	writer.append("<table style=\"width:100%\">");
         	writer.append("<tr>\n");
         	writer.append("<td width=\"15%\"></td>\n");
-        	writer.append("<td width=\"85%\">\n");
-            writer.append("<div onMouseOver=\"this.className='highlight'\" onMouseOut=\"this.className='normal'\" id=\"result\">\n");
+        	writer.append("<td width=\"55%\" >\n");
+        	//String s=item.summary;
+            writer.append(String.format("<div onMouseover=\"ddrivetip('%s','#990033')\"; onMouseout=\"hideddrivetip()\" id=\"result\">\n",item.summary));
             writer.append(String.format("<a href=\"document?identifier=%s\">%s</a><br/>" +
                                         "<div id=\"meta\">%s - %s</div>\n",
                                         item.identifier,
@@ -219,8 +292,9 @@ public class SearchWebHandler extends AbstractHandler {
                                         scrub(item.identifier),
                                         scrub(item.url)));
             writer.append("</div>\n");
-            writer.append(" <div class=\"tooltip_description\" style=\"display:none\" title=\"Item 1 Description\">th desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>");
+           // writer.append(" <div class=\"tooltip_description\" style=\"display:none\" title=\"Item 1 Description\">th desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>");
             writer.append("</td>\n");
+            writer.append("<td width=\"15%\"></td>\n");
             writer.append("</tr>");
             writer.append("</table>");
         }
@@ -306,35 +380,33 @@ public class SearchWebHandler extends AbstractHandler {
     public void writeStyle(PrintWriter writer) {
     	writer.write("<style type=\"text/css\">\n");
     	writer.write(".highlight { background-color: #C0C0C0 }");
-    	writer.write(".bgimg { background-image: url(/images/2.png); width:100% height:100%}");
     	writer.write("body  { font-family: Helvetica, sans-serif;}\n");
     	writer.write(".rounded {border-radius:15px;	-moz-border-radius:15px;-webkit-border-radius:15px;}");
     	writer.write(".lighter {width:95%;	height:50px;padding:40px 25px;}");
     	writer.write("input[type=submit], input[type=submit]:hover{position:relative; left:-85px;border:1px solid #adc5cf; background: -webkit-linear-gradient(top, #e4f1f9 0%,#d5e7f3 100%); color:#7da2aa;cursor: pointer;}");
         writer.write("img { border-style: none; }\n");
         writer.write("#box { solid #ccc; margin: 10px auto; width: 500px;\" }\n");
-        writer.write("#bg {  position: fixed;  top: 0;  left: 0;min-width: 100%;min-height: 100%;});");
         writer.write("#box a { font-size: small; text-decoration: none; }\n");
         writer.write("#box a:link { color: rgb(0, 93, 40); }\n");
         writer.write("#box a:visited { color: rgb(90, 93, 90); }\n");
         writer.write("#header { background: rgb(210, 233, 217); border: 1px solid #ccc; }\n");
-        writer.write("#result { padding: 10px 5px; max-width: 550px; }\n");
+        writer.write("#result { padding: 10px 5px; max-width: 550px; solid #ccc;}\n");
         writer.write("#meta { font-size: small; color: rgb(60, 100, 60); }\n");
         writer.write("#summary { font-size: small; }\n");
+        writer.write("#dhtmltooltip{position: absolute;width: 150px;border: 2px solid black;padding: 2px;background-color: lightyellow;visibility: hidden;z-index: 100;");
+        writer.write("#dhtmlpointer{position:absolute;left: -300px;z-index: 101;visibility: hidden;}");
         writer.write("#debug { display: none; }\n");
         writer.write("</style>");
     }
 
     public void handleMainPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
+        
         response.setContentType("text/html");
 
         writer.append("<html background-size: cover; >\n");
         writer.append("<head>\n");
         //writeStyle(writer);
-        writer.append("<script type=\"text/javascript\">");
-        writer.append("$('button').click(function() { $('#comment1').after($('<div id=\"comment2\"><p>Next comment</p><div class=\"rateit\"></div></div>'));  $('#comment2 .rateit').rateit();});");
-        writer.write("</script>");
         writer.write("<style type=\"text/css\">\n");
     	writer.write(".rounded {border-radius:15px;	-moz-border-radius:15px;-webkit-border-radius:15px;}");
     	writer.write(".lighter {width:95%;	height:50px;padding:40px 25px;}");
@@ -342,7 +414,6 @@ public class SearchWebHandler extends AbstractHandler {
     	writer.write("body  { font-family: Helvetica, sans-serif;  background-image: url(/images/timeMachine1.jpg);background-size:cover;background-repeat: no-repeat; fixed; }\n");
         writer.write("img { border-style: none; }\n");
         writer.write("#box { solid #ccc; margin: 0; display:inline-block; position:relative; }\n");
-        //writer.write("#bg {  display: block;position: absolute;top: 0;left: 0;cursor: pointer; height:100px;});");
         writer.write("#header { background: rgb(210, 233, 217); border: 1px solid #ccc; }\n");
         writer.write("</style>");
         writer.append("<title>Galago Search</title></head>");
@@ -350,16 +421,12 @@ public class SearchWebHandler extends AbstractHandler {
         
         writer.append("<center><br/><br/><div id=\"lighter\" ></a><br/>\n");
         writer.append("<form action=\"search\"><input type=\"text\" name=\"q\" class=\"search rounded\" size=\"80\" style=\"height: 40px\" placeholder=\"Enter your query and start the time machine :p \"><input value=\"Search\" class=\"search rounded\" type=\"submit\" style=\"height: 37px; width:80px\" ></span></form><br/><br/>");
-        writer.append("<div class=\"rateit\" data-rateit-value=\"2.5\" data-rateit-ispreset=\"true\" data-rateit-readonly=\"true\"></div>");
         writer.append("</div></center></body></html>\n");
         
         writer.close();
     }
 
-    public void handle(String target,
-            HttpServletRequest request,
-            HttpServletResponse response,
-            int dispatch) throws IOException, ServletException {
+    public void handle(String target,HttpServletRequest request, HttpServletResponse response,int dispatch) throws IOException, ServletException {
         if (request.getPathInfo().equals("/search")) {
             try {
                 handleSearch(request, response);
@@ -378,12 +445,18 @@ public class SearchWebHandler extends AbstractHandler {
             handleSnippet(request, response);
         } else if (request.getPathInfo().startsWith("/images")) {
             handleImage(request, response);
-        } else {
+        } else if(request.getPathInfo().equals("/js1")){
+        	handleJs(request,response);
+        } else if(request.getPathInfo().equals("/css1")){
+        	handleCs(request,response);
+        }else {
             handleMainPage(request, response);
         }
     }
 
-    private SearchResult performSearch(HttpServletRequest request) throws Exception {
+    
+
+	private SearchResult performSearch(HttpServletRequest request) throws Exception {
         String query = request.getParameter("q");
         String startAtString = request.getParameter("start");
         String countString = request.getParameter("n");
